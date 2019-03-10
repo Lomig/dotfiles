208b1e48fe4648cc09570b6b7b9c7e426a925c76
Lomig Enfroy <dunwael@gmail.com>
2019-03-10 21:24:43 +0100

    exit now detach the tmux session instead of closing it.

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -3,3 +3,12 @@ alias ta='tmux attach'
 alias tls='tmux ls'
 alias tat='tmux attach -t'
 alias tns='tmux new-session -s'
+
+# Detach instead of exit if tmux
+exit() {
+    if [[ -z $TMUX ]]; then
+        builtin exit
+    else
+        tmux detach
+    fi
+}
