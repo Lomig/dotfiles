064c3fbffb72445292a4659cd0779c52c43ae4ab
Lomig <dunwael@gmail.com>
2020-01-14 03:47:38 +0100

    Adding exa aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,6 +16,10 @@ alias .....="cd ../../../.."
 alias ls=' exa --group-directories-first'
 alias la=' ls -a'
 alias ll=' ls --git -l'
+alias lt=' ls --tree -D -L 2 -I ${TREE_IGNORE}'
+alias ltt=' ls --tree -D -L 3 -I ${TREE_IGNORE}'
+alias lttt=' ls --tree -D -L 4 -I ${TREE_IGNORE}'
+alias ltttt=' ls --tree -D -L 5 -I ${TREE_IGNORE}'
 
 alias rmf="rm -rf"
 
